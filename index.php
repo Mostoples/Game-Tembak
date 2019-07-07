d7469c82838f81e59002ebe17e13ed39c91a4bc5
Mostoples <mostoplesam@gmail.com>
2019-07-08 02:56:58 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome Page</title>
+	<link rel="stylesheet" href="style/style-base.css"/>
+    <link rel="stylesheet" href="style/style-login.css"/>
+</head>
+<body>
+		<div id='bg'></div>
+		
+		<div id='login-title-label' style='text-align : center; left : 10vw'>Welcome, Klik 'login' button to login to the game</div>		
+			<a href='login.php'><div id='login-button'></div></a>
+        
+</body>
+</html>
